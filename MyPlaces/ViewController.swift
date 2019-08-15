66084aaeea46375f8969b9b7a839a9a48a668e0d
Ruslan <ruslanfilistovich@gmail.com>
2019-08-15 11:55:11 +0300

    add ten row with content "Cell"

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -9,19 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+    }
+    
 
 
 }
