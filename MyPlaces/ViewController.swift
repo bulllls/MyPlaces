1eab489d1dbc898aa8ec66332216dfef8ab8993c
Ruslan <ruslanfilistovich@gmail.com>
2019-08-19 11:36:10 +0300

    add image array

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -29,7 +29,10 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
         cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        
         return cell
     }
     
