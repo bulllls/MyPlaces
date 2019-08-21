105590bd86d6214b8d37be7f1899b92c3cf1a22d
Ruslan <ruslanfilistovich@gmail.com>
2019-08-21 12:44:32 +0300

    Delete Action

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -41,6 +41,19 @@ class ViewController: UITableViewController {
 
         return cell
     }
+    
+    // MARK: Table view delegate
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let place = places[indexPath.row]
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (_, _) in
+            
+            StorageManager.deleteObject(place)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        return [deleteAction]
+    }
 
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         
